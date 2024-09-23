b24859520db448a348629f9573153dc43567ce3a
imtalhaozer <imtalhaozer@gmail.com>
2024-09-23 10:29:54 +0300

    car element updated

--- RentManagementApp/Entity/Car.cs
+++ RentManagementApp/Entity/Car.cs
@@ -1,15 +1,28 @@
-﻿namespace RentManagementApp.Entity;
+﻿using RentManagementApp.Repository;
 
-public record Car(
-    int Id, 
-    int ColorId, 
-    int FuelId, 
-    int TransmissionId, 
-    string CarState,
-    int? KiloMeter,
-    short? ModelYear, 
-    string? Plate,
-    string? BrandName,
-    string? ModelName,
-    double? DailyPrice
-    );
+namespace RentManagementApp.Entity;
+
+public sealed class Car:CommonInterface
+{
+    public Car()
+    {
+        
+    }
+    public int Id{ get; set; }
+    public int ColorId { get; set; }
+    public int FuelId { get; set; }
+    public int TransmissionId { get; set; }
+    public string? CarState { get; set; }
+    public int? KiloMeter { get; set; }
+    public short? ModelYear { get; set; }
+    public string? Plate { get; set; }
+    public string? BrandName { get; set; }
+    public string? ModelName { get; set; }
+    public double? DailyPrice { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id:{Id} ColorId:{ColorId} FuelId:{FuelId} TransmissionId:{TransmissionId} CarState:{CarState} " +
+               $"Kilometer:{KiloMeter} ModelYear:{ModelYear} Plate:{Plate} BrandName:{BrandName} ModelName:{ModelName} DailyPrice:{DailyPrice}";
+    }
+};
