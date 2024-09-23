83420213ee616b797b02184eb0929e943a15a7db
imtalhaozer <imtalhaozer@gmail.com>
2024-09-23 10:28:48 +0300

    New Transmission Service

--- RentManagementApp/Service/TransmissionService.cs
+++ RentManagementApp/Service/TransmissionService.cs
@@ -1,6 +1,51 @@
-﻿namespace RentManagementApp.Service;
+﻿using RentManagementApp.Entity;
+using RentManagementApp.Repository;
+
+namespace RentManagementApp.Service;
 
 public class TransmissionService
 {
+    private TransmissionRepository _transmissionRepo = new TransmissionRepository();
     
+    public void AddTransmission(Transmission transmission)
+    {
+        Transmission newTransmission = _transmissionRepo.Add(transmission);
+        Console.WriteLine($"Transmission added {newTransmission}");
+    }
+
+    public void GetAllTransmissions()
+    {
+        List<Transmission> transmissions = _transmissionRepo.GetAll();
+        transmissions.ForEach(x => Console.WriteLine($"Transmission: {x}"));
+    }
+
+    public void GetTransmissionById(int id)
+    {
+        Transmission? transmission = _transmissionRepo.GetById(id);
+        if (transmission is null)
+        {
+            Console.WriteLine($"Transmission cannot be found");
+            return;
+        }
+
+        Console.WriteLine(transmission);
+    }
+
+    public void UpdateTransmission(int id, Transmission transmission)
+    {
+        List<Transmission> updatedTransmission = _transmissionRepo.Update(id, transmission);
+        Console.WriteLine("Transmission updated");
+    }
+
+    public void DeleteTransmission(int id)
+    {
+        Transmission? transmission = _transmissionRepo.GetById(id);
+        if (transmission is null)
+        {
+            Console.WriteLine($"Transmission cannot be found");
+            return;
+        }
+        _transmissionRepo.Delete(id);
+        Console.WriteLine("Transmission deleted");
+    }
 }
